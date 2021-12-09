fa99f7e6486635ba59ebdcaa26cfb8fc5987b67e
Jostein Nordal Lysberg <jostein.lysberg@gmail.com>
2021-12-09 15:11:09 +0100

    Generate PaperView heatmap

--- Code/ETDataHub/ETDataHub/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Code/ETDataHub/ETDataHub/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -173,7 +173,7 @@ namespace ETDataHub {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ETDataHub;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ETDataHub;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
